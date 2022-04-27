846c00b3b27cd59a30095e01449e94c46d985acc
AydinTuna <tunaaydin6161@gmail.com>
2022-04-27 14:04:11 +0300

    v0.5

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log("Hello World");
+
+// Comments
+
+// Variables
+
+let age = 25
+console.log(age);
+
+const salary = 2000
+console.log(salary);
+
+// Data Types in JavaScript
+/*
+Primitives:            Non-Primitives:
+- String               - Objects
+- Number
+- Boolean
+- Undefined
+- Null
+- BigInt
+- Symbol
+*/
+
+const person = {
+    FirstName: "Bruce",
+    LastName: "Wayne",
+    Age: 30
+}
+console.log(person.FirstName);
+
+const oddNumbers = [1,2,343,4,54,5];
+console.log(oddNumbers[0]);
+
+// JavaScript is a dynamically typed language
